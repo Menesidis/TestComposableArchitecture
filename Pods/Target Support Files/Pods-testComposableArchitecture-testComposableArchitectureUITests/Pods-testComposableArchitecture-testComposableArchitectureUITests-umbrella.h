acd1fda0319acccd01479e54466a78ee5673dbe6
Menesidis <menesidis@gmail.com>
2020-11-27 20:33:50 +0200

    Pod install

---
+++ Pods/Target Support Files/Pods-testComposableArchitecture-testComposableArchitectureUITests/Pods-testComposableArchitecture-testComposableArchitectureUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_testComposableArchitecture_testComposableArchitectureUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_testComposableArchitecture_testComposableArchitectureUITestsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-testComposableArchitecture/Pods-testComposableArchitecture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testComposableArchitecture : NSObject
+@end
+@implementation PodsDummy_Pods_testComposableArchitecture
+@end
